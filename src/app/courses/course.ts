205438dd99171d528cb1a238704ae4d4dca45fb8
Romulo Saraiva <romulopsaraiva@gmail.com>
2020-03-09 22:27:05 -0300

    Criando classe curso

---
+++ src/app/courses/course.ts
@@ -0,0 +1,11 @@
+export class Course{
+    id: number;
+    name: string;
+    imageUrl: string;
+    prince: number;
+    code: string;
+    duration: number;
+    rating: number;
+    releaseDate: string;
+    
+}
