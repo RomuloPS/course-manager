1d158c2d98a0b487de9f638f7b0ee28165aa5570
Romulo Saraiva <romulopsaraiva@gmail.com>
2020-03-09 22:27:32 -0300

    Criando lista de cursos

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
 
 @Component({
     selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
 
+    courses: Course[] = [];
 
+    ngOnInit(): void{
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '/assets/images/forms.png',
+                prince: 99.99,
+                code: 'XPS-7891',
+                duration: 120,
+                rating: 5.4,
+                releaseDate: 'December, 4, 2019'
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '/assets/images/http.png',
+                prince: 45.99,
+                code: 'APS-5489',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'December, 8, 2019'
+            }
+        ]
+    }
 
 }
